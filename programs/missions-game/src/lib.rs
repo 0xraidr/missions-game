8754ee088a58014e15d4b05e614b188fb8cf7eae
raidr <decentralizedju@gmail.com>
2023-05-19 20:05:15 -0400

    deposit working

--- programs/missions-game/src/lib.rs
+++ programs/missions-game/src/lib.rs
@@ -1,3 +1,5 @@
+use anchor_spl::token::TokenAccount;
+
 use {
     anchor_lang::prelude::*,
     anchor_spl::{
@@ -5,6 +7,7 @@ use {
         associated_token,
     },
 };
+use anchor_spl::token::Token;
 
 declare_id!("Fdsd3qotXNtvEY9mnYKe5iZGfQVf6UHBqxX2X5d4HWCj");
 
@@ -12,31 +15,38 @@ declare_id!("Fdsd3qotXNtvEY9mnYKe5iZGfQVf6UHBqxX2X5d4HWCj");
 pub mod missions_game {
     use super::*;
 
- // function below is to choose a mission and deposit nft to play
-pub fn deposit_play(
-    ctx: Context<DepositPlay>, 
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.vault_state.owner = *ctx.accounts.owner.key;
+        ctx.accounts.vault_state.auth_bump = *ctx.bumps.get("vault_auth").unwrap();
+        ctx.accounts.vault_state.vault_bump = *ctx.bumps.get("vault").unwrap();
+        ctx.accounts.vault_state.score = 0;
+        Ok(())
+    }
+
+pub fn deposit(
+    ctx: Context<DepositSpl>, 
     quantity: u64,
-    mission_choice: u8,
+    // mission_choice: u8,
 ) -> Result<()> {
-    require!( mission_choice == 0 || mission_choice == 1, MissionError::InvalidMissionChoice);
+    // require!( mission_choice == 0 || mission_choice == 1, MissionError::InvalidMissionChoice);
 
-    let mission_choice_text = match mission_choice {
-        0 => "Mission 1",
-        1 => "Mission 2",
-        _ => "Error" //let's add error handle
-    }.to_string();
+    // let mission_choice_text = match mission_choice {
+    //     0 => "Mission 1",
+    //     1 => "Mission 2",
+    //     _ => "Error" //let's add error handle
+    // }.to_string();
 
-    msg!("Player chose {}", mission_choice_text);
+    // msg!("Player chose {}", mission_choice_text);
     msg!("Transferring token(s)...");
-    msg!("Mint: {}", &ctx.accounts.mint_account.to_account_info().key());   
-    msg!("From Token Address: {}", &ctx.accounts.from_associated_token_account.key());     
-    msg!("To Token Address: {}", &ctx.accounts.escrow_token_account.key());     
+    msg!("Mint: {}", &ctx.accounts.token_mint.to_account_info().key());   
+    msg!("From Token Address: {}", &ctx.accounts.owner_ata.key());     
+    msg!("To Token Address: {}", &ctx.accounts.vault_ata.key());     
     token::transfer(
         CpiContext::new(
             ctx.accounts.token_program.to_account_info(),
             token::Transfer {
-                from: ctx.accounts.from_associated_token_account.to_account_info(),
-                to: ctx.accounts.escrow_token_account.to_account_info(),
+                from: ctx.accounts.owner_ata.to_account_info(),
+                to: ctx.accounts.vault_ata.to_account_info(),
                 authority: ctx.accounts.owner.to_account_info(),
             },
         ),
@@ -48,108 +58,164 @@ pub fn deposit_play(
     Ok(())
 }
 
-// pub fn claim_results(
-//     ctx: Context<WithdrawClaim>, 
-//     quantity: u64,
-// ) -> Result<()> {
+pub fn withdraw(
+    ctx: Context<Withdraw>, 
+    quantity: u64,
+) -> Result<()> {
 
-//     msg!("Transferring tokens...");
-//     msg!("Mint: {}", &ctx.accounts.mint_account.to_account_info().key());   
-//     msg!("From Token Address: {}", &ctx.accounts.escrow_token_account.key());     
-//     msg!("To Token Address: {}", &ctx.accounts.to_associated_token_account.key());     
-//     token::transfer(
-//         CpiContext::new_with_signer(
-//             ctx.accounts.token_program.to_account_info(),
-//             token::Transfer {
-//                 from: ctx.accounts.escrow_token_account.to_account_info(),
-//                 to: ctx.accounts.to_associated_token_account.to_account_info(),
-//                 authority: ctx.accounts.owner.to_account_info(),
-//             },
-//             &[&[b"escrow", ctx.accounts.owner.key().as_ref(), &[*ctx.bumps.get("escrow").unwrap()]]]
-// ), 
-//         quantity,
-//     )?;
+    msg!("Transferring tokens...");
+    msg!("Mint: {}", &ctx.accounts.mint_account.to_account_info().key());   
+    msg!("From Token Address: {}", &ctx.accounts.vault.key());     
+    msg!("To Token Address: {}", &ctx.accounts.holders_associated_token_account.key());     
+    token::transfer(
+        CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            token::Transfer {
+                from: ctx.accounts.vault.to_account_info(),
+                to: ctx.accounts.holders_associated_token_account.to_account_info(),
+                authority: ctx.accounts.owner.to_account_info(),
+            },
+            &[&[b"vault", ctx.accounts.vault_auth.key().as_ref(), &[*ctx.bumps.get("vault").unwrap()]]]
+), 
+        quantity,
+    )?;
 
-//     // token::transfer(
-//     //     CpiContext::new(
-//     //         ctx.accounts.token_program.to_account_info(),
-//     //         token::Transfer {
-//     //             from: ctx.accounts.escrow_token_account.to_account_info(),
-//     //             to: ctx.accounts.to_associated_token_account.to_account_info(),
-//     //             authority: ctx.accounts.owner.to_account_info(),
-//     //         },
-//     //     ),
-//     //     quantity,
-//     // )?;
+    msg!("Tokens Withdrawn successfully.");
 
-//     msg!("Tokens Withdrawn successfully.");
-
-//     Ok(())
-// }
+    Ok(())
+}
 }
 
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    #[account(init, payer = owner, space = 8 + 32 + 3)]
+    pub vault_state: Account<'info, VaultState>,
+    #[account(seeds = [b"auth", vault_state.key().as_ref()], bump)]
+    ///CHECK: NO NEED TO CHECK THIS
+    pub vault_auth:  UncheckedAccount<'info,>,
+    #[account(mut, seeds = [b"vault", vault_auth.key().as_ref()], bump)]
+    pub vault: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
 
 #[derive(Accounts)]
-pub struct DepositPlay<'info> {
+pub struct DepositSpl<'info> {
+    #[account(mut)]
+    vault_state: Account<'info, VaultState>,
+    #[account(seeds = [b"auth", vault_state.key().as_ref()], bump)]
+    /// CHECK Dont need to check this
+    vault_auth: UncheckedAccount<'info>,
+    #[account(mut)]
+    owner_ata: Account<'info, TokenAccount>,
+    #[account(mut)]
+    vault_ata: Account<'info, TokenAccount>,
+    /// CHECK Dont need to check this
+    token_mint: UncheckedAccount<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    #[account(mut)]
+    owner: Signer<'info>
+}
+
+// #[derive(Accounts)]
+// pub struct Deposit<'info> {
+//     #[account(mut)]
+//     pub owner: Signer<'info>,
+//     #[account(mut)]
+//     pub mint_account: Account<'info, token::Mint>,
+//     #[account(mut)]
+//     pub holders_associated_token_account: Account<'info, token::TokenAccount>,
+//     #[account(init, payer = owner, space = 8 + 32 + 3)]
+//     pub vault_state: Account<'info, VaultState>,
+//     #[account(seeds = [b"auth", vault_state.key().as_ref()], bump)]
+//     ///CHECK: NO NEED TO CHECK THIS
+//     pub vault_auth:  UncheckedAccount<'info,>,
+//     #[account(mut, seeds = [b"vault", vault_auth.key().as_ref()], bump)]
+//     pub vault: Account<'info, token::TokenAccount>,
+//     pub system_program: Program<'info, System>,
+//     pub rent: Sysvar<'info, Rent>,
+//     pub token_program: Program<'info, token::Token>,
+//     pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
+// }
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
     #[account(mut)]
     pub mint_account: Account<'info, token::Mint>,
-    #[account(
-        mut,
-        // associated_token::mint = mint_account,
-        // associated_token::authority = owner,
-    )]
-    pub from_associated_token_account: Account<'info, token::TokenAccount>,
-    pub owner: SystemAccount<'info>,
-    #[account(
-        mut,
-        // init_if_needed,
-        // space = 8 + 8,
-        // payer = payer,
-        seeds=[b"escrow", payer.key().as_ref()], bump)]
-    pub escrow_token_account: Account<'info, token::TokenAccount>,
-    // pub recipient: SystemAccount<'info>,
-    #[account(mut)]
-    pub payer: Signer<'info>,
-    pub rent: Sysvar<'info, Rent>,
+    #[account(mut, associated_token::mint = mint_account, associated_token::authority = owner)]
+    pub holders_associated_token_account: Account<'info, token::TokenAccount>,
+    #[account(init, payer = owner, space = 8 + 32 + 3)]
+    pub vault_state: Account<'info, VaultState>,
+    #[account(seeds = [b"auth", vault_state.key().as_ref()], bump)]
+    ///CHECK: NO NEED TO CHECK THIS
+    pub vault_auth:  UncheckedAccount<'info,>,
+    #[account(mut, seeds = [b"vault", vault_auth.key().as_ref()], bump)]
+    pub vault: Account<'info, token::TokenAccount>,
     pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
     pub token_program: Program<'info, token::Token>,
     pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
 }
 
-// #[derive(Accounts)]
-// pub struct WithdrawClaim<'info> {
-//     #[account(mut)]
-//     pub mint_account: Account<'info, token::Mint>,
-//     #[account(
-//         mut,
-//         // init_if_needed,
-//         // payer = payer,
-//         // space = 8 + 8,
-//         seeds=[b"escrow",owner.key().as_ref()], bump
-//         // associated_token::mint = mint_account,
-//         // associated_token::authority = recipient,
-//     )]
-//     pub escrow_token_account: Account<'info, token::TokenAccount>,
-//     pub owner: SystemAccount<'info>,
-//     #[account(
-//         init_if_needed,
-//         payer = payer,
-//         associated_token::mint = mint_account,
-//         associated_token::authority = recipient,
-//     )]
-//     pub to_associated_token_account: Account<'info, token::TokenAccount>,
-//     pub recipient: SystemAccount<'info>,
-//     #[account(mut)]
-//     pub payer: Signer<'info>,
-//     pub rent: Sysvar<'info, Rent>,
-//     pub system_program: Program<'info, System>,
-//     pub token_program: Program<'info, token::Token>,
-//     pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
-// }
 
 
 #[error_code] 
 pub enum MissionError {
     #[msg("Choose a valid Mission!")]
     InvalidMissionChoice,
+}
+
+// #[derive(Accounts)]
+// pub struct Initialize<'info> {
+//     #[account(mut)]
+//     pub owner: Signer<'info>,
+//     #[account(init, payer = owner, space = 8 + 32 + 3)]
+//     pub vault_state: Account<'info, VaultState>,
+//     #[account(seeds = [b"auth", vault_state.key().as_ref()], bump)]
+//     ///CHECK: NO NEED TO CHECK THIS
+//     pub vault_auth:  UncheckedAccount<'info,>,
+//     #[account(mut, seeds = [b"vault", vault_auth.key().as_ref()], bump)]
+//     pub vault: SystemAccount<'info>,
+//     pub system_program: Program<'info, System>,
+// }
+
+// #[derive(Accounts)]
+// pub struct Deposit<'info> {
+//     #[account(mut, constraint = vault_state.owner == owner.key())]
+//     pub owner: Signer<'info>,
+//     #[account(mut)]
+//     pub vault_state: Account<'info, VaultState>,
+//     #[account(seeds = [b"auth", vault_state.key().as_ref()], bump)]
+//     ///CHECK: NO NEED TO CHECK THIS
+//     pub vault_auth:  UncheckedAccount<'info,>,
+//     #[account(mut, seeds = [b"vault", vault_auth.key().as_ref()], bump)]
+//     pub vault: SystemAccount<'info>,
+//     pub system_program: Program<'info, System>,
+// }
+
+// #[derive(Accounts)]
+// pub struct Withdraw<'info> {
+//     #[account(mut, constraint = vault_state.owner == owner.key())]
+//     pub owner: Signer<'info>,
+//     #[account(mut)]
+//     pub vault_state: Account<'info, VaultState>,
+//     #[account(seeds = [b"auth", vault_state.key().as_ref()], bump)]
+//     ///CHECK: NO NEED TO CHECK THIS
+//     pub vault_auth:  UncheckedAccount<'info,>,
+//     #[account(mut, seeds = [b"vault", vault_auth.key().as_ref()], bump)]
+//     pub vault: SystemAccount<'info>,
+//     pub system_program: Program<'info, System>,
+// }
+
+
+#[account]
+pub struct VaultState {
+    owner: Pubkey,
+    auth_bump: u8,
+    vault_bump: u8,
+    score: u8
 }
--- tests/missions-game.ts
+++ tests/missions-game.ts
@@ -3,15 +3,23 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@project-serum/anchor";
 import { MissionsGame } from "../target/types/missions_game";
 import * as Token from "@solana/spl-token";
-import PublicKey = anchor.web3.PublicKey;
-import Keypair = anchor.web3.Keypair;
 import * as utils from "../test_utils/utils";
 import chai, { assert, expect } from "chai";
 import chaiAsPromised from "chai-as-promised";
 import { wallet } from "./dev-wallet";
 import { ASSOCIATED_PROGRAM_ID } from "@project-serum/anchor/dist/cjs/utils/token";
-import { Connection } from "@solana/web3.js";
+import {
+  Connection,
+  Keypair,
+  SystemProgram,
+  PublicKey,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
 import * as spl from "@solana/spl-token";
+import {
+  TOKEN_PROGRAM_ID,
+  getOrCreateAssociatedTokenAccount,
+} from "@solana/spl-token";
 
 chai.use(chaiAsPromised);
 
@@ -20,26 +28,109 @@ describe("missions-game", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
   const program = anchor.workspace.MissionsGame as Program<MissionsGame>;
   const provider = utils.getProvider();
-  const getProgram = () =>
-    anchor.workspace.MissionsGame as Program<MissionsGame>;
   let connection = new Connection("https://api.devnet.solana.com");
-
-  const myKeypair = provider.wallet.payer as anchor.web3.Keypair;
-
-  // the nfts mint address
-  const nftMintAddy = new PublicKey(
-    "BpHoy9vkgM9873dT7N6dP7PfiLtKAbEyg8gihvpXPMdw"
-  );
 
   // We're going to import our keypair from the wallet file
   const owner = Keypair.fromSecretKey(new Uint8Array(wallet));
 
+  const vaultState = Keypair.generate();
+
+  // the nfts mint address
+  const nftMintAddy = new PublicKey(
+    "B3JJXuBHtyVUmfPAnTrfXijWimWLeUQnowTzgnT7ZRaz"
+  );
+
+  const vaultAuth_seeds = [
+    Buffer.from("auth"),
+    vaultState.publicKey.toBuffer(),
+  ];
+  const vaultAuth = PublicKey.findProgramAddressSync(
+    vaultAuth_seeds,
+    program.programId
+  )[0];
+
+  const vault_seeds = [Buffer.from("vault"), vaultAuth.toBuffer()];
+  const vault = PublicKey.findProgramAddressSync(
+    vault_seeds,
+    program.programId
+  )[0];
+
+  it("Is initialized!", async () => {
+    // try {
+    const txhash = await program.methods
+      .initialize()
+      .accounts({
+        owner: owner.publicKey,
+        vaultState: vaultState.publicKey,
+        vaultAuth: vaultAuth,
+        vault: vault,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([owner, vaultState])
+      .rpc();
+    console.log(`Success! Check out your TX here:
+        https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
+    // } catch (e) {
+    //   console.error(`Oops, something went wrong: ${e}`);
+    // }
+  });
+
   it("Deposit NFT and choose a Mission!", async () => {
     // REMEMBER!!! THE "ALLOW OWNER OFF CURVE" BOOLEAN PARAMETER!!! THIS IS TO GET OR CREATE AN ATA FROM A PDA!!!
-    // const escrowPdaPubkey = utils.getOurPda("escrow", owner)[0];
-    const [pda] = await PublicKey.findProgramAddressSync(
-      [Buffer.from("escrow"), owner.publicKey.toBuffer()],
+
+    let vaultAta = await spl.getOrCreateAssociatedTokenAccount(
+      connection,
+      owner,
+      nftMintAddy,
+      vaultAuth,
+      true
+    );
+
+    let ownerAta = await spl.getOrCreateAssociatedTokenAccount(
+      connection,
+      owner,
+      nftMintAddy,
+      owner.publicKey
+    );
+
+    const sx = await program.methods
+      .deposit(new anchor.BN(1))
+      .accounts({
+        owner: owner.publicKey,
+        vaultState: vaultState.publicKey,
+        vaultAuth,
+        systemProgram: SystemProgram.programId,
+        ownerAta: ownerAta.address,
+        vaultAta: vaultAta.address,
+        tokenMint: nftMintAddy,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      })
+      .signers([owner])
+      .rpc();
+
+    console.log(`"vaultAta: ", ${vaultAta.address}`);
+    console.log(`"vaultPubkey: ", ${vault.toBase58()}`);
+  });
+
+  it("Confirm Addresses!", async () => {
+    const [pda] = PublicKey.findProgramAddressSync(
+      [Buffer.from("escrow"), owner.publicKey.toBytes()],
       program.programId
+    );
+
+    let vaultAta = await spl.getOrCreateAssociatedTokenAccount(
+      connection,
+      owner,
+      nftMintAddy,
+      vault,
+      true
+    );
+
+    let ownerAta = await spl.getOrCreateAssociatedTokenAccount(
+      connection,
+      owner,
+      nftMintAddy,
+      owner.publicKey
     );
 
     let escrowAta = await spl.getOrCreateAssociatedTokenAccount(
@@ -49,36 +140,10 @@ describe("missions-game", () => {
       pda,
       true
     );
-
-    const fromAssociatedTokenAccountAddress =
-      await anchor.utils.token.associatedAddress({
-        mint: nftMintAddy,
-        owner: owner.publicKey,
-      });
-    const toEscrowTokenAccount = await anchor.utils.token.associatedAddress({
-      mint: nftMintAddy,
-      owner: escrowAta.address,
-    });
-
-    const sx = await program.methods
-      .depositPlay(new anchor.BN(1), 0)
-      .accounts({
-        mintAccount: nftMintAddy,
-        fromAssociatedTokenAccount: fromAssociatedTokenAccountAddress,
-        owner: owner.publicKey,
-        escrowTokenAccount: escrowAta.address,
-        payer: owner.publicKey,
-        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-        systemProgram: anchor.web3.SystemProgram.programId,
-        tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
-        associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
-      })
-      .signers([owner])
-      .rpc();
-
-    console.log("Success!");
-    console.log(`   Mint Address: ${nftMintAddy}`);
-    console.log(`   Tx Signature: ${sx}`);
-    console.log("Senders Wallet: ", owner.publicKey);
+    console.log(` HODLERS PUBKEY: ${owner.publicKey}`);
+    console.log(` PDA PUBKEY: ${pda.toBase58()}`);
+    console.log(`"vaultAta: ", ${vaultAta.address}`);
+    console.log(`"vaultPubkey: ", ${vault.toBase58()}`);
+    console.log(` HOLDERS ATA: ${ownerAta.address.toBase58()}`);
   });
 });
